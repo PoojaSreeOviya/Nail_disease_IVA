bfcea503e084c251002a93e130c8cdfecc083ff1
Pooja Sree <125353843+PoojaSreeOviya@users.noreply.github.com>
2024-10-18 22:25:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import numpy as np
+import tensorflow as tf
+from flask import Flask, request, jsonify
+import cv2
+from pathlib import Path
+
+app = Flask(__name__)
+
+# Load the trained model
+model = tf.keras.models.load_model('nail_disease_model.keras')
+
+# Define the class names
+class_names = ['healthy', 'onychomycosis', 'psoriasis']
+
+img_height, img_width = 224, 224  # Image size for MobileNetV3
+
+def load_and_preprocess_image(image):
+    image_resized = cv2.resize(image, (img_height, img_width))
+    image_array = np.expand_dims(image_resized, axis=0)
+    return image_array
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['file']
+    image = np.fromstring(file.read(), np.uint8)
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    
+    # Preprocess the image
+    processed_image = load_and_preprocess_image(image)
+    
+    # Predict the class
+    pred = model.predict(processed_image)
+    predicted_class = np.argmax(pred)
+
+    # Return the predicted class as a response
+    return jsonify({
+        'predicted_class': class_names[predicted_class]
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
